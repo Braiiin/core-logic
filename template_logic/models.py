58b483c10626ac48b68e254844e1ebd85013a4c0
Alvin Wan <hi@alvinwan.com>
2015-09-07 16:20:54 -0700

    added employment, service APIs

--- template_logic/models.py
+++ template_logic/models.py
@@ -1,23 +1,32 @@
 from logic.v1.models import Document, db
+from logic.v1.core.models import User
 
 
-class Sample(Document):
-	"""
-	Models are defined using Flask-Mongoengine. For all options, see
-	flask-mongoengine.readthedocs.org or mongoengine.readthedocs.org
-	
-	*Note*: If parameters here have required=True and args are auto-generated
-	using model.fields_to_args(), the API will require that parameter in the 
-	API call.
-	
-	To ameliorate the API issue, you have one of two options:
-	- override options:
-		model.fields_to_args(override={'required': False})
-	- override the argument:
-		model.fields_to_args(required=Arg(str, required=False))
-	"""
-	
-	options = ['a', 'b', 'c', 'd']
-	
-	option = db.StringField(choices=options)
-	required = db.IntField(default=10, required=True)
+class Service(Document):
+    """represents a Braiiin service"""
+
+    statuses = Document.choices(
+        'private beta',
+        'public beta',
+        'launch',
+        'legacy'
+    )
+
+    name = db.StringField()
+    status = db.StringField(choices=statuses)
+
+
+class Employment(Document):
+    """represents a user's affiliation with a service"""
+
+    statuses = Document.choices(
+        'not approved',
+        'approved',
+        'suspended'
+    )
+
+    user = db.ReferenceField(User)
+    service = db.ReferenceField(Service)
+    status = db.StringField(choices=statuses)
+    privilege = db.StringField(default='user')
+    position = db.StringField(default='user')
