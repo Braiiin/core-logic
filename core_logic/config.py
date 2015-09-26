749b23a5c6487cd6f102dbaf7a7e5fcba7991a2b
Alvin Wan <hi@alvinwan.com>
2015-09-26 11:02:24 -0700

    updated prod conf

---
+++ core_logic/config.py
@@ -0,0 +1,47 @@
+"""
+
+Configurations
+--------------
+
+Various setups for different app instances
+
+"""
+
+
+class Config:
+    """Default config"""
+
+    DEBUG = False
+    TESTING = False
+    SESSION_STORE = 'session'
+    MONGODB_DB = 'default'
+    SECRET_KEY = 'flask+braiiin=<3'
+    LIVE = ['v1']
+    STATIC_PATH = 'static'
+    HASHING_ROUNDS = 15
+
+    INIT = {
+        'port': 8001,
+        'host': '127.0.0.1',
+    }
+
+
+class ProductionConfig(Config):
+    """Production vars"""
+
+    INIT = {
+        'port': 80,
+        'host': '127.0.0.1',
+    }
+
+
+class DevelopmentConfig(Config):
+    """For local runs"""
+    DEBUG = True
+    MONGODB_DB = 'dev'
+
+
+class TestConfig(Config):
+    """For automated testing"""
+    TESTING = True
+    MONGODB_DB = 'test'
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # the main Flask application
 from core_logic import create_core_app
 
-app = create_core_app()
+app = create_core_app(root='core_logic')
 
 if __name__ == "__main__":
     app.run(**app.config['INIT'])
